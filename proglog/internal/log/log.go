6b0d1928148a4484f1e12a284a3576a02fe7fd21
Michael Street <5597260+MStreet3@users.noreply.github.com>
2022-03-05 14:33:43 -0500

    add log tests

--- proglog/internal/log/log.go
+++ proglog/internal/log/log.go
@@ -1,6 +1,12 @@
 package log
 
 import (
+	"io"
+	"io/ioutil"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
 	"sync"
 
 	api "github.com/mstreet3/proglog/api/v1"
@@ -15,10 +21,53 @@ type Log struct {
 }
 
 func NewLog(dir string, c Config) (*Log, error) {
-	return &Log{}, nil
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+	return l, l.setup()
 }
 
+// Boostraps a Log from the given directory
 func (l *Log) setup() error {
+	files, err := ioutil.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+
+	// Collect the offset from the .store file names
+	var baseOffsets []uint64
+	for _, f := range files {
+		ext := path.Ext(f.Name())
+		if ext != ".index" {
+			offStr := strings.TrimSuffix(f.Name(), ext)
+			off, _ := strconv.ParseUint(offStr, 10, 0)
+			baseOffsets = append(baseOffsets, off)
+		}
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+
+	// Create a new segment for each offset
+	for i, _ := range baseOffsets {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+	}
+
+	// Create at least one new segment if the directory is empty
+	if l.segments == nil {
+		if err = l.newSegment(l.Config.Segment.InitialOffset); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -48,6 +97,10 @@ func (l *Log) LowestOffset() (uint64, error) {
 
 func (l *Log) HighestOffset() (uint64, error) {
 	return 0, nil
+}
+
+func (l *Log) Reader() io.Reader {
+	return io.MultiReader()
 }
 
 func (l *Log) Truncate(lowest uint64) error {
--- proglog/internal/log/log_test.go
+++ proglog/internal/log/log_test.go
@@ -1,7 +1,116 @@
 package log
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
 
-type logTestFn func(testing.T, *Log)
+	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/require"
 
-func TestLog(t *testing.T) {}
+	api "github.com/mstreet3/proglog/api/v1"
+)
+
+type logTestFn func(*testing.T, *Log)
+
+var _append = &api.Record{
+	Value: []byte("hello world"),
+}
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]logTestFn{
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"init with existing segments":       testInitExisting,
+		"reader":                            testReader,
+		"truncate":                          testTruncate,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := ioutil.TempDir("", "store-test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+
+			fn(t, log)
+		})
+	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+
+	off, err := log.Append(_append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, _append.Value, read.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
+}
+
+func testInitExisting(t *testing.T, log *Log) {
+	var (
+		minOff = uint64(0)
+		maxOff = uint64(2)
+	)
+
+	// Append to log and close it
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(_append)
+		require.NoError(t, err)
+	}
+	require.NoError(t, log.Close())
+
+	// Validate lowest and highest offset
+	validateOffsets(t, log, minOff, maxOff)
+
+	// Create a new log and verify its lowest and highest offset
+	n, err := NewLog(log.Dir, log.Config)
+	require.NoError(t, err)
+	validateOffsets(t, n, minOff, maxOff)
+
+}
+
+func validateOffsets(t *testing.T, log *Log, min, max uint64) {
+	off, err := log.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, min, off)
+	off, err = log.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, max, off)
+}
+
+func testReader(t *testing.T, log *Log) {
+	off, err := log.Append(_append)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	reader := log.Reader()
+	b, err := ioutil.ReadAll(reader)
+	require.NoError(t, err)
+
+	read := &api.Record{}
+	err = proto.Unmarshal(b[lenWidth:], read)
+	require.NoError(t, err)
+	require.Equal(t, _append.Value, read.Value)
+}
+
+func testTruncate(t *testing.T, log *Log) {
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(_append)
+		require.NoError(t, err)
+	}
+	err := log.Truncate(1)
+	require.NoError(t, err)
+
+	_, err = log.Read(0)
+	require.Error(t, err)
+}
